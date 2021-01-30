5dd659942809662a8e7d7cb66086811d10c03338
LegateBlake <blakewreed@gmail.com>
2021-01-30 15:16:22 -0600

    Initial commit for SW5E to Ord Mantell Nights excel format. Added conversion for characteristics and abilities on the "Combat and Skills" sheet.

---
+++ Program.cs
@@ -0,0 +1,204 @@
+﻿using Newtonsoft.Json;
+using OfficeOpenXml;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace SW5E_Converter
+{
+    class Program
+    {
+        public static string JSON_FILE_NAME = "gil.json";
+        public static string EXCEL_FILE_NAME = "test.xlsx";
+        static void Main(string[] args)
+        {
+            using StreamReader r = new StreamReader(JSON_FILE_NAME);
+            string json = r.ReadToEnd();
+            Rootobject jsonObject = JsonConvert.DeserializeObject<Rootobject>(json);
+
+            var fileInfo = new FileInfo(EXCEL_FILE_NAME);
+            if (File.Exists(EXCEL_FILE_NAME))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (var excelPackage = new OfficeOpenXml.ExcelPackage(fileInfo))
+                {
+                    ExcelWorksheet combatSheet = excelPackage.Workbook.Worksheets[1];
+                    ExcelWorksheet characterSheet = excelPackage.Workbook.Worksheets[2];
+                    ExcelWorksheet inventorySheet = excelPackage.Workbook.Worksheets[3];
+                    ExcelWorksheet powersSheet = excelPackage.Workbook.Worksheets[4];
+
+                    writeToCombatSheet(combatSheet, jsonObject);
+                    //writeToCharacterSheet(characterSheet, jsonObject);
+                    //writeToInventorySheet(inventorySheet, jsonObject);
+                    //writeToPowersSheet(powersSheet, jsonObject);
+                    excelPackage.Save();
+                }
+                Console.WriteLine(jsonObject.classes[0].name);
+            }
+        }
+
+        private static void writeToPowersSheet(ExcelWorksheet powersSheet, Rootobject rootobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void writeToInventorySheet(ExcelWorksheet inventorySheet, Rootobject rootobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void writeToCharacterSheet(ExcelWorksheet characterSheet, Rootobject rootobject)
+        {
+            
+        }
+
+        private static void writeToCombatSheet(ExcelWorksheet combatSheet, Rootobject rootobject)
+        {
+            var cells = combatSheet.Cells;
+            cells[5, 2].Value = rootobject.name;
+            for (int i = 0; i < rootobject.classes.Length && i <= 4; i++)
+            {
+                writeString(cells[3 + i, 19], rootobject.classes[i].name);
+                cells[3 + i, 26].Value = rootobject.classes[i].levels;
+                cells[3 + i, 28].Style.Fill.BackgroundColor.SetColor(Color.LightYellow);
+                cells[3 + i, 32].Style.Fill.BackgroundColor.SetColor(Color.LightYellow);
+            }
+
+            cells[1, 37].Value = rootobject.characteristics.alignment;
+            cells[1, 43].Value = rootobject.species.name;
+            cells[1, 56].Value = rootobject.background.name;
+            cells[3, 37].Value = rootobject.experiencePoints;
+            cells[3, 47].Style.Fill.BackgroundColor.SetColor(Color.LightYellow);
+            if (string.IsNullOrWhiteSpace(rootobject.user))
+            { 
+                cells[3, 58].Style.Fill.BackgroundColor.SetColor(Color.LightYellow);
+            }
+            else 
+            { 
+                cells[3, 58].Value = rootobject.user; 
+            }
+
+            cells[10, 2].Value = rootobject.baseAbilityScores.Strength;
+            cells[14, 2].Value = rootobject.baseAbilityScores.Dexterity;
+            cells[20, 2].Value = rootobject.baseAbilityScores.Constitution;
+            cells[24, 2].Value = rootobject.baseAbilityScores.Intelligence;
+            cells[32, 2].Value = rootobject.baseAbilityScores.Wisdom;
+            cells[40, 2].Value = rootobject.baseAbilityScores.Charisma;
+
+            writeAbilityScores(cells, rootobject);
+        }
+
+        private static void writeString(ExcelRange cell, string writeObject)
+        {
+            if (string.IsNullOrWhiteSpace(writeObject))
+            {
+                cell.Style.Fill.BackgroundColor.SetColor(Color.LightYellow);
+            }
+            else
+            {
+                cell.Value = writeObject;
+            }
+        }
+
+        private static void writeAbilityScores(ExcelRange cells, Rootobject rootobject)
+        {
+            var abilityScores = rootobject.tweaks.abilityScores;
+            cells[10, 2].Value = rootobject.baseAbilityScores.Strength + rootobject.species.abilityScoreImprovement.Strength;
+            cells[14, 2].Value = rootobject.baseAbilityScores.Dexterity + rootobject.species.abilityScoreImprovement.Dexterity;
+            cells[20, 2].Value = rootobject.baseAbilityScores.Constitution + rootobject.species.abilityScoreImprovement.Constitution;
+            cells[24, 2].Value = rootobject.baseAbilityScores.Intelligence + +rootobject.species.abilityScoreImprovement.Intelligence;
+            cells[32, 2].Value = rootobject.baseAbilityScores.Wisdom + rootobject.species.abilityScoreImprovement.Wisdom;
+            cells[40, 2].Value = rootobject.baseAbilityScores.Charisma + rootobject.species.abilityScoreImprovement.Charisma;
+            if (abilityScores != null)
+            {
+                Ability strength = abilityScores.Strength;
+                if (strength != null)
+                {
+                    writeSavingThrow(cells, strength.savingThrowModifier, 9);
+                    Skills skills = strength.skills;
+                    writeSkill(cells, skills.Athletics, 10);
+                }
+                Ability dexterity = abilityScores.Dexterity;
+                if (dexterity != null)
+                {
+                    writeSavingThrow(cells, dexterity.savingThrowModifier, 13);
+                    Skills skills = dexterity.skills;
+                    writeSkill(cells, skills.Acrobatics, 14);
+                    writeSkill(cells, skills.SleightofHand, 15);
+                    writeSkill(cells, skills.Stealth, 16);
+                }
+                Ability constitution = abilityScores.Constitution;
+                if (constitution != null)
+                {
+                    writeSavingThrow(cells, constitution.savingThrowModifier, 19);
+                }
+                Ability intelligence = abilityScores.Intelligence;
+                if (intelligence != null)
+                {
+                    writeSavingThrow(cells, intelligence.savingThrowModifier, 23);
+                    Skills skills = intelligence.skills;
+                    writeSkill(cells, skills.Investigation, 24);
+                    writeSkill(cells, skills.Lore, 25);
+                    writeSkill(cells, skills.Nature, 26);
+                    writeSkill(cells, skills.Piloting, 27);
+                    writeSkill(cells, skills.Technology, 28);
+                }
+                Ability wisdom = abilityScores.Wisdom;
+                if (wisdom != null)
+                {
+                    writeSavingThrow(cells, wisdom.savingThrowModifier, 31);
+                    Skills skills = wisdom.skills;
+                    writeSkill(cells, skills.AnimalHandling, 32);
+                    writeSkill(cells, skills.Insight, 33);
+                    writeSkill(cells, skills.Medicine, 34);
+                    writeSkill(cells, skills.Perception, 35);
+                    writeSkill(cells, skills.Survival, 36);
+                }
+                Ability charisma = abilityScores.Charisma;
+                if (charisma != null)
+                {
+                    writeSavingThrow(cells, charisma.savingThrowModifier, 39);
+                    Skills skills = charisma.skills;
+                    writeSkill(cells, skills.Deception, 40);
+                    writeSkill(cells, skills.Intimidation, 41);
+                    writeSkill(cells, skills.Performance, 42);
+                    writeSkill(cells, skills.Persuasion, 43);
+                }
+            }
+        }
+
+        static void writeSavingThrow(ExcelRange cells, Savingthrowmodifier savingthrowmodifier, int cellStart)
+        {
+            if (savingthrowmodifier != null)
+            {
+                if (savingthrowmodifier.proficiency == "Proficient")
+                {
+                    cells[cellStart, 11].Value = "•";
+                }
+                if (savingthrowmodifier.bonus != null)
+                {
+                    cells[cellStart, 13].Value = savingthrowmodifier.bonus;
+                }
+            }
+        }
+
+        static void writeSkill(ExcelRange cells, Skill skill, int cellStart)
+        {
+            if (skill != null)
+            {
+                if (skill.proficiency == "Proficient")
+                {
+                    cells[cellStart, 11].Value = "●";
+                }
+                else if (skill.proficiency == "Expertise")
+                {
+                    cells[cellStart, 11].Value = "x2";
+                }
+                if (skill.bonus != null)
+                {
+                    cells[cellStart, 13].Value = skill.bonus;
+                }
+            }
+        }
+    }
+}
---
+++ Rootobject.cs
@@ -0,0 +1,239 @@
+﻿using Newtonsoft.Json;
+
+namespace SW5E_Converter
+{
+
+    public class Rootobject
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string userId { get; set; }
+        public string builderVersion { get; set; }
+        public string image { get; set; }
+        public string user { get; set; }
+        public int experiencePoints { get; set; }
+        public Species species { get; set; }
+        public Class1[] classes { get; set; }
+        public Baseabilityscores baseAbilityScores { get; set; }
+        public Background background { get; set; }
+        public Characteristics characteristics { get; set; }
+        public int credits { get; set; }
+        public Equipment[] equipment { get; set; }
+        public Currentstats currentStats { get; set; }
+        public Tweaks tweaks { get; set; }
+        public Customproficiency[] customProficiencies { get; set; }
+        public string[] customLanguages { get; set; }
+        public Customfeature[] customFeatures { get; set; }
+        public string[] customFeats { get; set; }
+        public string[] customTechPowers { get; set; }
+        public object[] customForcePowers { get; set; }
+        public Customequipment[] customEquipment { get; set; }
+        public Settings settings { get; set; }
+        public string notes { get; set; }
+        public long createdAt { get; set; }
+        public long changedAt { get; set; }
+        public string localId { get; set; }
+    }
+
+    public class Species
+    {
+        public string name { get; set; }
+        public int abilityScoreImprovementSelectedOption { get; set; }
+        public Abilityscoreimprovement abilityScoreImprovement { get; set; }
+    }
+
+    public class Abilityscoreimprovement
+    {
+        public int Strength { get; set; } 
+        public int Dexterity { get; set; } 
+        public int Constitution { get; set; } 
+        public int Intelligence { get; set; } 
+        public int Wisdom { get; set; } 
+        public int Charisma { get; set; } 
+    }
+
+    public class Baseabilityscores
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+    }
+
+    public class Background
+    {
+        public string name { get; set; }
+        public Feat feat { get; set; }
+        public string feature { get; set; }
+    }
+
+    public class Feat
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Characteristics
+    {
+        public string alignment { get; set; }
+        public string PersonalityTraits { get; set; }
+        public string Ideal { get; set; }
+        public string Bond { get; set; }
+        public string Flaw { get; set; }
+        public string Gender { get; set; }
+        public string PlaceofBirth { get; set; }
+        public string Age { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string Hair { get; set; }
+        public string Eyes { get; set; }
+        public string Skin { get; set; }
+        public string Appearance { get; set; }
+        public string Backstory { get; set; }
+    }
+
+    public class Currentstats
+    {
+        public int hitPointsLost { get; set; }
+        public int temporaryHitPoints { get; set; }
+        public int techPointsUsed { get; set; }
+        public int forcePointsUsed { get; set; }
+        public int superiorityDiceUsed { get; set; }
+        public Hitdiceused hitDiceUsed { get; set; }
+        public Deathsaves deathSaves { get; set; }
+        public bool hasInspiration { get; set; }
+        public Featurestimesused featuresTimesUsed { get; set; }
+        public object[] conditions { get; set; }
+        public int exhaustion { get; set; }
+        public Highlevelcasting highLevelCasting { get; set; }
+    }
+
+    public class Hitdiceused
+    {
+    }
+
+    public class Deathsaves
+    {
+        public int successes { get; set; }
+        public int failures { get; set; }
+    }
+
+    public class Featurestimesused
+    {
+    }
+
+    public class Highlevelcasting
+    {
+        public bool level6 { get; set; }
+        public bool level7 { get; set; }
+        public bool level8 { get; set; }
+        public bool level9 { get; set; }
+    }
+
+    public class Tweaks
+    {
+        public Abilityscores abilityScores { get; set; }
+    }
+
+    public class Abilityscores
+    {
+        public Ability Strength { get; set; }
+        public Ability Dexterity { get; set; }
+        public Ability Constitution { get; set; }
+        public Ability Intelligence { get; set; }
+        public Ability Wisdom { get; set; }
+        public Ability Charisma { get; set; }
+    }
+
+    public class Ability
+    {
+        public Savingthrowmodifier savingThrowModifier { get; set; }
+        public Skills skills { get; set; }
+    }
+
+    public class Savingthrowmodifier
+    {
+        public string proficiency { get; set; }
+        public int _override { get; set; }
+        public string bonus { get; set; }
+    }
+
+    public class Skills
+    {
+        public Skill Athletics { get; set; }
+        public Skill Acrobatics { get; set; }
+        [JsonProperty(PropertyName = "Sleight of Hand")]
+        public Skill SleightofHand { get; set; }
+        public Skill Stealth { get; set; }
+        public Skill Investigation { get; set; }
+        public Skill Lore { get; set; }
+        public Skill Nature { get; set; }
+        public Skill Piloting { get; set; }
+        public Skill Technology { get; set; }
+        [JsonProperty(PropertyName = "Animal Handling")]
+        public Skill AnimalHandling { get; set; }
+        public Skill Insight { get; set; }
+        public Skill Medicine { get; set; }
+        public Skill Perception { get; set; }
+        public Skill Survival { get; set; }
+        public Skill Deception { get; set; }
+        public Skill Intimidation { get; set; }
+        public Skill Performance { get; set; }
+        public Skill Persuasion { get; set; }
+    }
+
+    public class Skill
+    {
+        public string bonus { get; set; }
+        public int _override { get; set; }
+        public string proficiency { get; set; }
+    }
+
+    public class Settings
+    {
+        public bool isEnforcingForcePrerequisites { get; set; }
+        public bool isFixedHitPoints { get; set; }
+        public string abilityScoreMethod { get; set; }
+    }
+
+    public class Class1
+    {
+        public string name { get; set; }
+        public int levels { get; set; }
+        public object[] hitPoints { get; set; }
+        public object[] abilityScoreImprovements { get; set; }
+        public string[] forcePowers { get; set; }
+    }
+
+    public class Equipment
+    {
+        public string name { get; set; }
+        public int quantity { get; set; }
+        public string category { get; set; }
+    }
+
+    public class Customproficiency
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string proficiencyLevel { get; set; }
+    }
+
+    public class Customfeature
+    {
+        public string name { get; set; }
+        public string content { get; set; }
+    }
+
+    public class Customequipment
+    {
+        public string name { get; set; }
+        public int quantity { get; set; }
+        public string equipmentCategory { get; set; }
+        public int cost { get; set; }
+        public string description { get; set; }
+        public int weight { get; set; }
+    }
+}
